79314e7b63ccf759c9078192552b0947051427c8
apillet <apillet@gmail.com>
2016-05-13 14:32:25 -0300

    Add first solution

--- lib/prime_factors_kata.rb
+++ lib/prime_factors_kata.rb
@@ -1,3 +1,35 @@
 module PrimeFactorsKata
-  # Your code goes here...
+  def self.solve value
+    aux = value
+    factors = []
+
+    while aux > 1 do
+      factor = next_prime_factor(aux)
+      aux = aux / factor
+
+      factors << factor
+    end
+
+    factors
+  end
+
+  def self.is_prime? value
+    n = value.abs
+
+    return true  if n == 2
+    return false if n.modulo(2).zero? || n < 2
+
+    # Use 3 because its the smallest prime number next to 2
+    3.step(Math.sqrt(n).to_i, 2) do |x|
+      return false if n.modulo(x).zero?
+    end
+
+    return true
+  end
+
+  def self.next_prime_factor value
+    (2..value).detect do |f|
+      is_prime?(f) && value.modulo(f) == 0
+    end
+  end
 end
--- spec/prime_factors_kata_spec.rb
+++ spec/prime_factors_kata_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 describe PrimeFactorsKata do
+  it "should known when a number is prime" do
+    # Primes
+    expect(PrimeFactorsKata.is_prime?(2)).to be_truthy
+    expect(PrimeFactorsKata.is_prime?(3)).to be_truthy
+    expect(PrimeFactorsKata.is_prime?(5)).to be_truthy
+    expect(PrimeFactorsKata.is_prime?(7)).to be_truthy
+    expect(PrimeFactorsKata.is_prime?(23)).to be_truthy
+
+    # Not Primes
+    expect(PrimeFactorsKata.is_prime?(8)).to be_falsey
+    expect(PrimeFactorsKata.is_prime?(25)).to be_falsey
+    expect(PrimeFactorsKata.is_prime?(9)).to be_falsey
+  end
+
+  it "should known the next prime factor" do
+    expect(PrimeFactorsKata.next_prime_factor(8)).to eq(2)
+    expect(PrimeFactorsKata.next_prime_factor(7)).to eq(7)
+  end
+
+  it "should return prime factors" do
+    expect(PrimeFactorsKata.solve(2)).to eq([2])
+    expect(PrimeFactorsKata.solve(3)).to eq([3])
+    expect(PrimeFactorsKata.solve(4)).to eq([2, 2])
+    expect(PrimeFactorsKata.solve(6)).to eq([2, 3])
+    expect(PrimeFactorsKata.solve(8)).to eq([2, 2, 2])
+    expect(PrimeFactorsKata.solve(9)).to eq([3, 3])
+  end
 end
